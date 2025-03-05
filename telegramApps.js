48a774037d3d02fd78181d4509fc58c5e9ebf15e
Pijamka88 <111456100+Pijamka88@users.noreply.github.com>
2025-03-05 13:30:29 +0300

    Update telegramApps.js

--- telegramApps.js
+++ telegramApps.js
@@ -1,13 +1,13 @@
-
+ 
 // Initialize Telegram Web App
 const tg = window.Telegram.WebApp;
-
+ 
 // Expand the web app to full height
 tg.expand();
-
+ 
 // Setting theme
 document.body.className = tg.colorScheme;
-
+ 
 // Function to send score to bot
 function sendScoreToBot(score, size) {
     // Get user info if available
@@ -21,23 +21,14 @@ function sendScoreToBot(score, size) {
         timestamp: new Date().toISOString()
     };
     
-    // You can use this to send data to your backend
-    fetch('/game-data', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Telegram-Init-Data': tg.initData
-        },
-        body: JSON.stringify(gameData)
-    })
-    .then(response => response.json())
-    .then(data => console.log('Success:', data))
-    .catch(error => console.error('Error:', error));
+    // On GitHub Pages we can only send data directly to Telegram
+    // We can't make a server request since there is no backend
+    console.log('Sending game data to Telegram:', gameData);
     
-    // You can also use this to send data back to the Telegram bot
+    // Send data back to the Telegram bot
     tg.sendData(JSON.stringify(gameData));
 }
-
+ 
 // Function to show game completion in Telegram
 function completeGame(score, size) {
     // Send final score to bot
@@ -46,7 +37,7 @@ function completeGame(score, size) {
     // Close the Web App and send the data back
     tg.close();
 }
-
+ 
 // Telegram button to show in the game
 function showTelegramBackButton() {
     tg.BackButton.show();
@@ -57,12 +48,12 @@ function showTelegramBackButton() {
         }
     });
 }
-
+ 
 // Hide the back button when not needed
 function hideTelegramBackButton() {
     tg.BackButton.hide();
 }
-
+ 
 // Telegram haptic feedback
 function telegramHapticFeedback(type) {
     switch(type) {
@@ -77,3 +68,4 @@ function telegramHapticFeedback(type) {
             break;
     }
 }
+ 
